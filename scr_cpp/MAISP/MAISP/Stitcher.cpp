ee417d656352d4ebe504c2e7ca024cfc3a2ab722
WitchEllen <rxwang_biz@outlook.com>
2020-09-29 10:06:33 +0800

    5nd

--- scr_cpp/MAISP/MAISP/Stitcher.cpp
+++ scr_cpp/MAISP/MAISP/Stitcher.cpp
@@ -1,27 +1,32 @@
-#include <stdio.h>  
-#include <iostream>  
 #include <vector>  
+#include "mStitcher.h"  
+#include "opencv2/stitching.hpp"
 
-#include "opencv2/highgui/highgui.hpp"  
-#include "opencv2/stitching.hpp" 
 using namespace cv;
 using namespace std;
 
 int main() {
 
-    Mat img_1 = imread("1.jpg");
-    Mat img_2 = imread("2.jpg");
+    Mat img_1 = imread("D:/Program/GitHub/MAISP/data/left.jpg");
+    Mat img_2 = imread("D:/Program/GitHub/MAISP/data/right.jpg");
+    Mat img_3 = imread("D:/Program/GitHub/MAISP/data/left2.jpg");
+    Mat img_4 = imread("D:/Program/GitHub/MAISP/data/right2.jpg");
 
+    vector<Mat> imgs_init;
     vector<Mat> imgs;
 
-    imgs.push_back(img_1);
-    imgs.push_back(img_2);
+    imgs_init.push_back(img_1);
+    imgs_init.push_back(img_2);
+    imgs.push_back(img_3);
+    imgs.push_back(img_4);
 
     Mat pano;
     Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA);
     stitcher->estimateTransform(imgs);
-    //stitcher.composePanorama(pano); //use old images
-    stitcher->composePanorama(imgs, pano); //use new images with same size and count
-    imwrite("pano.jpg", pano);
+    stitcher->composePanorama(pano); //use old images
+    //stitcher->composePanorama(imgs, pano); //use new images with same size and count
+    imwrite("D:/Program/GitHub/MAISP/result/pano.jpg", pano);
+    mStitch(img_4, img_3, "D:/Program/GitHub/MAISP/result/");
     return 0;
 }
+
