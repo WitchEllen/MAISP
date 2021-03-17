179c4ba8994d355c4d85d9bd085ceeed62d8f6f3
WitchEllen <rxwang_biz@outlook.com>
2021-03-17 15:25:08 +0800

    ...

--- scr_cpp/MAISP/MAISP/Stitcher.cpp
+++ scr_cpp/MAISP/MAISP/Stitcher.cpp
@@ -1,32 +1,64 @@
-#include <vector>  
+#include <vector>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <filesystem>
 #include "mStitcher.h"  
 #include "opencv2/stitching.hpp"
 
 using namespace cv;
 using namespace std;
+namespace fs = std::filesystem;
+
+void getAllPaths(string path, vector<string>& files, string format)
+{
+    transform(format.begin(), format.end(), format.begin(), ::tolower);
+    regex fileSuffix_l("(.*)(." + format + ")");
+    transform(format.begin(), format.end(), format.begin(), ::toupper);
+    regex fileSuffix_u("(.*)(." + format + ")");
+    for (const auto& entry : fs::directory_iterator(path))
+    {
+        fs::path path = entry.path();
+        if (std::regex_match(path.string(), fileSuffix_l) || std::regex_match(path.string(), fileSuffix_u))
+        {
+            files.push_back(path.string());
+        }
+    }
+}
 
 int main() {
+    fstream setting;
+    setting.open("setting.txt", ios::in);
 
-    Mat img_1 = imread("D:/Program/GitHub/MAISP/data/left.jpg");
-    Mat img_2 = imread("D:/Program/GitHub/MAISP/data/right.jpg");
-    Mat img_3 = imread("D:/Program/GitHub/MAISP/data/left2.jpg");
-    Mat img_4 = imread("D:/Program/GitHub/MAISP/data/right2.jpg");
+    string input_dir, output_path, name;
+    getline(setting, input_dir);
+    getline(setting, output_path);
+    getline(setting, name);
+    input_dir = input_dir + name + "/";
+
+    vector<string> files_path;
+    getAllPaths(input_dir, files_path, ".jpg");
 
     vector<Mat> imgs_init;
     vector<Mat> imgs;
 
-    imgs_init.push_back(img_1);
-    imgs_init.push_back(img_2);
-    imgs.push_back(img_3);
-    imgs.push_back(img_4);
+    for (auto file_path: files_path)
+        imgs.push_back(imread(file_path));
 
     Mat pano;
     Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA);
-    stitcher->estimateTransform(imgs);
-    stitcher->composePanorama(pano); //use old images
-    //stitcher->composePanorama(imgs, pano); //use new images with same size and count
-    imwrite("D:/Program/GitHub/MAISP/result/pano.jpg", pano);
-    mStitch(img_4, img_3, "D:/Program/GitHub/MAISP/result/");
+    //stitcher->stitch(imgs, pano);
+    //stitcher->estimateTransform(imgs);
+    //stitcher->composePanorama(pano); //use old images
+    //imwrite(output_path + name + "_m.png", pano);
+    //stitcher->setBlender(Blender::createDefault(Blender::FEATHER, true));
+    //stitcher->stitch(imgs, pano);
+    //imwrite(output_path + name + "_f.png", pano);
+    //stitcher->setBlender(Blender::createDefault(Blender::NO, true));
+    //stitcher->stitch(imgs, pano);
+    //imwrite(output_path + name + ".png", pano);
+    // stitcher->composePanorama(imgs, pano); //use new images with same size and count
+    pano = stitch(imgs);
+    imwrite(output_path + name + "_n_nomask.png", pano);
     return 0;
 }
-
--- scr_cpp/MAISP/MAISP/mStitcher.h
+++ scr_cpp/MAISP/MAISP/mStitcher.h
@@ -1,194 +1,182 @@
 #pragma once
-#include <iostream>
+#include "opencv2/core/core.hpp"
 #include "opencv2/highgui.hpp"
-#include "opencv2/calib3d.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/flann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/features2d/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
+#include "opencv2/xfeatures2d/nonfree.hpp"
+//#include "opencv2/legacy/legacy.hpp"
 
-using namespace std;
+#include "opencv2/stitching/detail/autocalib.hpp"
+#include "opencv2/stitching/detail/blenders.hpp"
+#include "opencv2/stitching/detail/camera.hpp"
+#include "opencv2/stitching/detail/exposure_compensate.hpp"
+#include "opencv2/stitching/detail/matchers.hpp"
+#include "opencv2/stitching/detail/motion_estimators.hpp"
+#include "opencv2/stitching/detail/seam_finders.hpp"
+#include "opencv2/stitching/detail/util.hpp"
+#include "opencv2/stitching/detail/warpers.hpp"
+#include "opencv2/stitching/warpers.hpp"
+
+#include <iostream>
+
 using namespace cv;
+using namespace std;
+using namespace cv::detail;
 using namespace cv::xfeatures2d;
 
-typedef struct
+Mat stitch(vector<Mat> imgs)
 {
-    Point2f left_top;
-    Point2f left_bottom;
-    Point2f right_top;
-    Point2f right_bottom;
-}four_corners_t;
+    int num_images = imgs.size();
 
-void OptimizeSeam(Mat& img1, Mat& trans, Mat& dst, four_corners_t& corners);
+    // old version
+    // Ptr<FeaturesFinder> finder;
+    // finder = new SurfFeaturesFinder();
 
-void CalcCorners(const Mat& H, const Mat& src, four_corners_t& corners)
-{
-    double v2[] = { 0, 0, 1 };  //左上角
-    double v1[3];   //变换后的坐标值
-    Mat V2 = Mat(3, 1, CV_64FC1, v2);   //列向量
-    Mat V1 = Mat(3, 1, CV_64FC1, v1);   //列向量
-
-    //左上角(0,0,1)
-    V1 = H * V2;
-    cout << "V2: " << V2 << endl;
-    cout << "V1: " << V1 << endl;
-    corners.left_top.x = v1[0] / v1[2];
-    corners.left_top.y = v1[1] / v1[2];
-
-    //左下角(0,src.rows,1)
-    v2[0] = 0;
-    v2[1] = src.rows;
-    v2[2] = 1;
-    V2 = Mat(3, 1, CV_64FC1, v2);   //列向量
-    V1 = Mat(3, 1, CV_64FC1, v1);   //列向量
-    V1 = H * V2;
-    corners.left_bottom.x = v1[0] / v1[2];
-    corners.left_bottom.y = v1[1] / v1[2];
-
-    //右上角(src.cols,0,1)
-    v2[0] = src.cols;
-    v2[1] = 0;
-    v2[2] = 1;
-    V2 = Mat(3, 1, CV_64FC1, v2);   //列向量
-    V1 = Mat(3, 1, CV_64FC1, v1);   //列向量
-    V1 = H * V2;
-    corners.right_top.x = v1[0] / v1[2];
-    corners.right_top.y = v1[1] / v1[2];
-
-    //右下角(src.cols,src.rows,1)
-    v2[0] = src.cols;
-    v2[1] = src.rows;
-    v2[2] = 1;
-    V2 = Mat(3, 1, CV_64FC1, v2);   //列向量
-    V1 = Mat(3, 1, CV_64FC1, v1);   //列向量
-    V1 = H * V2;
-    corners.right_bottom.x = v1[0] / v1[2];
-    corners.right_bottom.y = v1[1] / v1[2];
-}
-
-void mStitch(Mat& image01, Mat& image02, String dst_dir)
-{
-    //灰度图转换
-    Mat image1, image2;
-    cvtColor(image01, image1, COLOR_BGR2GRAY);
-    cvtColor(image02, image2, COLOR_BGR2GRAY);
-
-    //提取特征点并计算特征   
-    Ptr<SURF> Detector = SURF::create(2000);
-    vector<KeyPoint> keyPoint1, keyPoint2;
-    Mat imageDesc1, imageDesc2;
-    Detector->detectAndCompute(image1, Mat(), keyPoint1, imageDesc1);
-    Detector->detectAndCompute(image2, Mat(), keyPoint2, imageDesc2);
-
-    FlannBasedMatcher matcher1, matcher2;
-    vector<vector<DMatch> > matchePoints;
-    vector<DMatch> GoodMatchePoints;
-    vector<Mat> train_desc1(1, imageDesc1);
-    vector<Mat> train_desc2(1, imageDesc2);
-    matcher1.add(train_desc1);
-    matcher2.add(train_desc2);
-    set<pair<int,int> > matches;
-
-    matcher1.train();
-    matcher1.knnMatch(imageDesc2, matchePoints, 2);
-    // Lowe's algorithm,获取优秀匹配点
-    for (int i = 0; i < matchePoints.size(); i++)
-    {
-        if (matchePoints[i][0].distance < 0.4 * matchePoints[i][1].distance)
-        {
-            GoodMatchePoints.push_back(matchePoints[i][0]);
-            matches.insert(make_pair(matchePoints[i][0].queryIdx, matchePoints[i][0].trainIdx));
-        }
-    }
-    cout << endl << "1->2 matches: " << GoodMatchePoints.size() << endl;
-
-    matchePoints.clear();
-    matcher2.train();
-    matcher2.knnMatch(imageDesc1, matchePoints, 2);
-    // Lowe's algorithm,获取优秀匹配点
-    for (int i = 0; i < matchePoints.size(); i++)
-    {
-        if (matchePoints[i][0].distance < 0.4 * matchePoints[i][1].distance)
-        {
-            if (matches.find(make_pair(matchePoints[i][0].trainIdx, matchePoints[i][0].queryIdx)) == matches.end())
-            {
-                GoodMatchePoints.push_back(DMatch(matchePoints[i][0].trainIdx, matchePoints[i][0].queryIdx, matchePoints[i][0].distance));
-            }
-        }
-    }
-    cout << "1->2 & 2->1 matches: " << GoodMatchePoints.size() << endl;
-
-    Mat match_result;
-    drawMatches(image02, keyPoint2, image01, keyPoint1, GoodMatchePoints, match_result);
-    imwrite(dst_dir + "match_result.jpg", match_result);
-
-    vector<Point2f> imagePoints1, imagePoints2;
-
-    for (int i = 0; i < GoodMatchePoints.size(); i++)
-    {
-        imagePoints2.push_back(keyPoint2[GoodMatchePoints[i].queryIdx].pt);
-        imagePoints1.push_back(keyPoint1[GoodMatchePoints[i].trainIdx].pt);
+    Ptr<SURF> finder = SURF::create();  // SURF
+    vector<ImageFeatures> features(num_images);
+    for (int i = 0; i < num_images; i++) {
+        // (*finder)(imgs[i], features[i]); // old version
+        computeImageFeatures(finder, imgs[i], features[i]);
     }
 
-    //获取图像1到图像2的投影映射矩阵 尺寸为3*3  
-    Mat homo = findHomography(imagePoints1, imagePoints2, RANSAC);
+    vector<MatchesInfo> pairwise_matches;               //特征匹配信息
+    BestOf2NearestMatcher matcher(false, 0.3f, 6, 6);   //定义特征匹配器，2NN方法
+    matcher(features, pairwise_matches);                //进行特征匹配
 
-    //计算配准图的四个顶点坐标
-    four_corners_t corners;
-    CalcCorners(homo, image01, corners);
-    cout << "left_top:" << corners.left_top << endl;
-    cout << "left_bottom:" << corners.left_bottom << endl;
-    cout << "right_top:" << corners.right_top << endl;
-    cout << "right_bottom:" << corners.right_bottom << endl;
+    HomographyBasedEstimator estimator;                 //定义参数评估器
+    vector<CameraParams> cameras;                       //表示相机参数
+    estimator(features, pairwise_matches, cameras);     //进行相机参数评估
 
-    //图像配准  
-    Mat imageTransform1, imageTransform2;
-    warpPerspective(image01, imageTransform1, homo, Size(MAX(corners.right_top.x, corners.right_bottom.x), image02.rows));
-    //imshow("直接经过透视矩阵变换", imageTransform1);
-    imwrite(dst_dir + "trans1.jpg", imageTransform1);
-
-    //创建拼接后的图,需提前计算图的大小
-    int dst_width = imageTransform1.cols;  //取最右点的长度为拼接图的长度
-    int dst_height = image02.rows;
-
-    Mat dst(dst_height, dst_width, CV_8UC3);
-    dst.setTo(0);
-
-    imageTransform1.copyTo(dst(Rect(0, 0, imageTransform1.cols, imageTransform1.rows)));
-    image02.copyTo(dst(Rect(0, 0, image02.cols, image02.rows)));
-    imwrite(dst_dir + "b_dst.jpg", dst);
-
-    OptimizeSeam(image02, imageTransform1, dst, corners);
-    imwrite(dst_dir + "dst.jpg", dst);
-}
-
-//优化两图的连接处，使得拼接自然
-void OptimizeSeam(Mat& img1, Mat& trans, Mat& dst, four_corners_t& corners)
-{
-    int start = MIN(corners.left_top.x, corners.left_bottom.x);//开始位置，即重叠区域的左边界  
-    double processWidth = img1.cols - start;//重叠区域的宽度  
-    int rows = dst.rows;
-    int cols = img1.cols; //注意，是列数*通道数
-    double alpha = 1;//img1中像素的权重  
-    for (int i = 0; i < rows; i++)
+    for (size_t i = 0; i < cameras.size(); ++i)         //转换相机旋转参数的数据类型
     {
-        uchar* p = img1.ptr<uchar>(i);  //获取第i行的首地址
-        uchar* t = trans.ptr<uchar>(i);
-        uchar* d = dst.ptr<uchar>(i);
-        for (int j = start; j < cols; j++)
-        {
-            //如果遇到图像trans中无像素的黑点，则完全拷贝img1中的数据
-            if (t[j * 3] == 0 && t[j * 3 + 1] == 0 && t[j * 3 + 2] == 0)
-            {
-                alpha = 1;
-            }
-            else
-            {
-                //img1中像素的权重，与当前处理点距重叠区域左边界的距离成正比，实验证明，这种方法确实好  
-                alpha = (processWidth - (j - start)) / processWidth;
-            }
-            d[j * 3] = p[j * 3] * alpha + t[j * 3] * (1 - alpha);
-            d[j * 3 + 1] = p[j * 3 + 1] * alpha + t[j * 3 + 1] * (1 - alpha);
-            d[j * 3 + 2] = p[j * 3 + 2] * alpha + t[j * 3 + 2] * (1 - alpha);
-        }
+        Mat R;
+        cameras[i].R.convertTo(R, CV_32F);
+        cameras[i].R = R;
     }
+
+    Ptr<detail::BundleAdjusterBase> adjuster;           //光束平差法，精确相机参数
+    adjuster = new detail::BundleAdjusterReproj();      //重映射误差方法
+    //adjuster = new detail::BundleAdjusterRay();       //射线发散误差方法
+
+    adjuster->setConfThresh(1);                         //设置匹配置信度，该值设为1
+    (*adjuster)(features, pairwise_matches, cameras);   //精确评估相机参数
+
+    vector<Mat> rmats;
+    for (size_t i = 0; i < cameras.size(); ++i)         //复制相机的旋转参数
+        rmats.push_back(cameras[i].R.clone());
+    waveCorrect(rmats, WAVE_CORRECT_HORIZ);             //进行波形校正
+    for (size_t i = 0; i < cameras.size(); ++i)         //相机参数赋值
+        cameras[i].R = rmats[i];
+    rmats.clear();
+
+    vector<Point> corners(num_images);      //表示映射变换后图像的左上角坐标
+    vector<UMat> masks_warped(num_images);  //表示映射变换后的图像掩码
+    vector<UMat> images_warped(num_images); //表示映射变换后的图像
+    vector<Size> sizes(num_images);         //表示映射变换后的图像尺寸
+    vector<UMat> masks(num_images);         //表示源图的掩码
+
+    for (int i = 0; i < num_images; ++i)    //初始化源图的掩码
+    {
+        masks[i].create(imgs[i].size(), CV_8U);     //定义尺寸大小
+        masks[i].setTo(Scalar::all(255));           //全部赋值为255，表示源图的所有区域都使用
+    }
+
+    Ptr<WarperCreator> warper_creator;                  //定义图像映射变换创造器
+    //warper_creator = new cv::PlaneWarper();           //平面投影
+    //warper_creator = new cv::CylindricalWarper();     //柱面投影
+    warper_creator = new cv::SphericalWarper();       //球面投影
+    //warper_creator = new cv::FisheyeWarper();         //鱼眼投影
+    //warper_creator = new cv::StereographicWarper();   //立方体投影
+
+    //定义图像映射变换器，设置映射的尺度为相机的焦距，所有相机的焦距都相同
+    Ptr<RotationWarper> warper = warper_creator->create(static_cast<float>(cameras[0].focal));
+    for (int i = 0; i < num_images; ++i)
+    {
+        Mat_<float> K;
+        cameras[i].K().convertTo(K, CV_32F);    //转换相机内参数的数据类型
+        //对当前图像镜像投影变换，得到变换后的图像以及该图像的左上角坐标
+        corners[i] = warper->warp(imgs[i], K, cameras[i].R, INTER_LINEAR, BORDER_REFLECT, images_warped[i]);
+        sizes[i] = images_warped[i].size();     //得到尺寸
+        //得到变换后的图像掩码
+        warper->warp(masks[i], K, cameras[i].R, INTER_NEAREST, BORDER_CONSTANT, masks_warped[i]);
+    }
+
+    imgs.clear();
+    masks.clear();
+
+    // 创建曝光补偿器，应用增益补偿方法
+    Ptr<ExposureCompensator> compensator = ExposureCompensator::createDefault(ExposureCompensator::GAIN);
+    compensator->feed(corners, images_warped, masks_warped);    //得到曝光补偿器
+    for (int i = 0; i < num_images; ++i)    //应用曝光补偿器，对图像进行曝光补偿
+    {
+        compensator->apply(i, corners[i], images_warped[i], masks_warped[i]);
+    }
+
+    //在后面，我们还需要用到映射变换图的掩码masks_warped，因此这里为该变量添加一个副本masks_seam
+    vector<UMat> masks_seam(num_images);
+    for (int i = 0; i < num_images; i++)
+        masks_warped[i].copyTo(masks_seam[i]);
+
+    Ptr<SeamFinder> seam_finder;                                //定义接缝线寻找器
+   //seam_finder = new NoSeamFinder();                          //无需寻找接缝线
+   //seam_finder = new VoronoiSeamFinder();                     //逐点法
+   //seam_finder = new DpSeamFinder(DpSeamFinder::COLOR);       //动态规范法
+   //seam_finder = new DpSeamFinder(DpSeamFinder::COLOR_GRAD);
+   //图割法
+   //seam_finder = new GraphCutSeamFinder(GraphCutSeamFinder::COST_COLOR);
+    seam_finder = new GraphCutSeamFinder(GraphCutSeamFinder::COST_COLOR_GRAD);
+
+    vector<UMat> images_warped_f(num_images);
+    for (int i = 0; i < num_images; ++i)    //图像数据类型转换
+        images_warped[i].convertTo(images_warped_f[i], CV_32F);
+
+    images_warped.clear();
+
+    //得到接缝线的掩码图像masks_seam
+    seam_finder->find(images_warped_f, corners, masks_seam);
+
+    vector<Mat> images_warped_s(num_images);
+    Ptr<Blender> blender;    //定义图像融合器
+
+    blender = Blender::createDefault(Blender::NO, false);    //简单融合方法
+    //羽化融合方法
+    //blender = Blender::createDefault(Blender::FEATHER, false);
+    //FeatherBlender* fb = dynamic_cast<FeatherBlender*>(static_cast<Blender*>(blender));
+    //fb->setSharpness(0.005);    //设置羽化锐度
+
+    //blender = Blender::createDefault(Blender::MULTI_BAND, false);    //多频段融合
+    //MultiBandBlender* mb = dynamic_cast<MultiBandBlender*>(static_cast<Blender*>(blender));
+    //mb->setNumBands(8);   //设置频段数，即金字塔层数
+
+    blender->prepare(corners, sizes);    //生成全景图像区域
+
+    //在融合的时候，最重要的是在接缝线两侧进行处理，而上一步在寻找接缝线后得到的掩码的边界就是接缝线处，因此我们还需要在接缝线两侧开辟一块区域用于融合处理，这一处理过程对羽化方法尤为关键
+    //应用膨胀算法缩小掩码面积
+    vector<Mat> dilate_img(num_images);
+    Mat element = getStructuringElement(MORPH_RECT, Size(20, 20));    //定义结构元素
+    for (int k = 0; k < num_images; k++)
+    {
+        images_warped_f[k].convertTo(images_warped_s[k], CV_16S);    //改变数据类型
+        dilate(masks_seam[k], masks_seam[k], element);    //膨胀运算
+        //映射变换图的掩码和膨胀后的掩码相“与”，从而使扩展的区域仅仅限于接缝线两侧，其他边界处不受影响
+        // masks_seam[k] = masks_seam[k] & masks_warped[k];
+        //Mat mask_seam, mask_warped;
+        //masks_seam[k].copyTo(mask_seam);
+        //masks_warped[k].copyTo(mask_warped);
+        //mask_seam = mask_seam & mask_warped;
+        //mask_seam.copyTo(masks_seam[k]);
+        blender->feed(images_warped_s[k], masks_seam[k], corners[k]);    //初始化数据
+    }
+
+    masks_seam.clear();
+    images_warped_s.clear();
+    masks_warped.clear();
+    images_warped_f.clear();
+
+    Mat result, result_mask;
+    //完成融合操作，得到全景图像result和它的掩码result_mask
+    blender->blend(result, result_mask);
+
+    return result;
 }
